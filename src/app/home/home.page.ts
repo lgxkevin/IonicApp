c557fb3f8c9da974347ac8804f2a166951a44507
lgxkevin <lgx9587@gmail.com>
2019-05-13 12:38:09 +1200

    update login page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -9,15 +10,37 @@ export class HomePage {
 
   username: string;
   password: string;
-  constructor() {
+  isLoginSuccess: boolean;
+  constructor(
+    public toastController: ToastController
+  ) {
   }
 
   loginCheck() {
     console.log(`Username: ${this.username},password: ${this.password}`);
     if (this.username === 'kevin' && this.password === 'qwer1234') {
       console.log('Login successfully');
+      this.isLoginSuccess = true;
+      this.presentToast('Login successfully');
     } else {
       console.log('Login error');
+      this.isLoginSuccess = false;
+      this.presentToast('Login error');
     }
+  }
+  async presentToast(message: string) {
+    const feedbackMessage = message;
+    let toastColor: string;
+    if (feedbackMessage === 'Login successfully') {
+      toastColor = 'success';
+    } else {
+      toastColor = 'danger';
+    }
+    const toast = await this.toastController.create({
+      message: feedbackMessage,
+      duration: 2000,
+      color: toastColor
+    });
+    toast.present();
   }
 }
